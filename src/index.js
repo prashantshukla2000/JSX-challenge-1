6a31aa167e6dfa04fde812236989f65abed15db4
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-21 09:12:55 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Best Veg Options:</h1>
+    <ul>
+      <li>Paneer</li>
+      <li>Mushroom</li>
+      <li>Aloo bujiya</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
